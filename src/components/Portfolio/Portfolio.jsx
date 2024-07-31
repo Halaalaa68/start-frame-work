b3823c9e47a110ae821612df4d6712360b05b573
Hala Alaa Eldin <66278563+Halaalaa68@users.noreply.github.com>
2024-07-31 14:25:31 +0300

    Update Portfolio.jsx

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -8,7 +8,7 @@ export default function Portfolio() {
   return (
     <div className="w-full mt-28 lg:pb-28 pb-96 md:pt-10 pt-28 mb-60 flex flex-col justify-center items-center">
       <h1 className='uppercase text-[40px] mt-4 md:mt-10 font-bold text-center'>portfolio component</h1>
-      <Star color={'text-cyan-950'} star={'star2'}/>
+      <Star color={'text-cyan-900'} star={'star2'}/>
       <div className='flex flex-wrap gap-9 container m-auto justify-center items-center'>
         <div className='md:w-1/3 xl:w-1/4 w-80 relative group rounded-lg'>
           <img src={p1} className='w-full rounded-lg' alt="" />
